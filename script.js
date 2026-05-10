111c033dbedf8da658821acaca5f354f2aead292
milene-lobato <millene.lobato.if@gmail.com>
2026-05-10 13:06:41 -0300

    Modificações para otimizar o codigo

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
+const envelope = document.querySelector(".envelope");
+const musica = document.getElementById("musica");
+const botao = document.getElementById("playMusic");
+const video = document.getElementById("video");
+
+let tocando = false;
+let coracoesAtivos = false;
+
+// ABRIR CARTA
 function abrirCarta() {
-  const envelope = document.querySelector(".envelope");
+
   envelope.classList.toggle("aberto");
+
+  // inicia corações só uma vez
+  if (!coracoesAtivos) {
+
+    setInterval(criarCoracao, 300);
+
+    coracoesAtivos = true;
+  }
 }
 
-document.getElementById("playMusic").addEventListener("click", function() {
-  document.getElementById("musica").play();
+// PLAY E PAUSE DA MÚSICA
+botao.addEventListener("click", () => {
+
+  if (tocando) {
+
+    musica.pause();
+    botao.innerHTML = "▶ Tocar música";
+
+  } else {
+
+    musica.play();
+    botao.innerHTML = "⏸ Pausar música";
+  }
+
+  tocando = !tocando;
 });
+
+// QUANDO O VÍDEO TOCAR
+video.addEventListener("play", () => {
+
+  musica.pause();
+
+  tocando = false;
+
+  botao.innerHTML = "▶ Tocar música";
+});
+
+// QUANDO O VÍDEO TERMINAR
+video.addEventListener("ended", () => {
+
+  musica.play();
+
+  tocando = true;
+
+  botao.innerHTML = "⏸ Pausar música";
+});
+
+// CRIAR CORAÇÕES
+function criarCoracao() {
+
+  const heart = document.createElement("div");
+
+  heart.classList.add("heart");
+
+  heart.innerHTML = "💖";
+
+  heart.style.left = Math.random() * 100 + "vw";
+
+  heart.style.fontSize =
+    Math.random() * 20 + 15 + "px";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
